9793e6d90773b5dde326f3319f083b38f42d0918
Stepan Razdobarov <northernpeople@gmail.com>
2018-04-05 16:05:54 -0600

    Todo.js exporting default and additional value, app.js importing default and additional value

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -1,10 +1,11 @@
 // assembles the app
-import Dodo from "./model/Todo";
+import Dodo, {someText} from "./model/Todo"; // imports someText in addition to default.
 
 
 class App{
   constructor(){
     new Dodo("first todo argument");
+    console.log("also can access someText imported from Todo.js : " + someText);
   }
 }
 
--- app/scripts/src/model/Todo.js
+++ app/scripts/src/model/Todo.js
@@ -1,6 +1,9 @@
-export default class Todo{
+export default  class Todo{
   constructor(description){
     this.description = description;
     console.log("created: ",this);
   }
 }
+
+let someText = "textVariableValue";
+export {someText}; // export, in addition to default
